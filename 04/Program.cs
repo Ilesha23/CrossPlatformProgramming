62b8fd0f5118adcad2b637898ee5b36882f41753
Ilya Yakovlev <Illia.Yakovliev@cs.khpi.edu.ua>
2022-04-19 15:31:57 +0300

    03

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _02;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,16 +12,20 @@ namespace _01
         static void Main(string[] args)
         {
             Milk milk = new Milk("Milk", true, 5, 1.0, 7, 30.0m);
-            Console.WriteLine(milk.ToString());
-
             Cheese cheese = new Cheese("Cheese", false, 30, true, 50.0, 0.5, 100.0m);
-            Console.WriteLine(cheese.ToString());
-
             Chocolate chocolate = new Chocolate("Choc", false, true, true, 50.0, 500, 20.0m);
-            Console.WriteLine(chocolate.ToString());
-
             Cocoa cocoa = new Cocoa("Cocoa", false, true, true, 500, 30.0m);
-            Console.WriteLine(cocoa.ToString());
+
+            Container c = new Container();
+            c.push(milk);
+            c.push(cheese);
+            c.push(chocolate);
+            c.push(cocoa);
+
+            Console.WriteLine(c[0].ToString());
+            Console.WriteLine(c["Cheese"].ToString());
+            Console.WriteLine(c[20.0m].ToString());
         }
     }
 }
+
